6fb33f6b755ecdb5a1bb378c5cc068585bf167d2
jsn-lps <lpsjsn94@gmail.com>
2023-02-07 17:12:17 -0500

    cleaning

--- public/favorite.js
+++ public/favorite.js
@@ -13,16 +13,17 @@ starIcon.src = 'https://www.shareicon.net/data/256x256/2015/08/30/93083_star_512
 starIcon.classList.add('starIcon')
 let streamersDiv;
 let vcount;
+let streamerNum = 0;
 // select all live streamers div
 setTimeout(() => streamersDiv = document.querySelector('[aria-label="Followed Channels"]').children[1].children[0].children[0].children[0], 5000);
 
 // set viewer count div for placement reference
-setTimeout(() => vcount = document.querySelector('[aria-label="Followed Channels"]').children[1].children[0].children[0].children[0].children[0].children[1].children[1], 5000);
+setTimeout(() => vcount = document.querySelector('[aria-label="Followed Channels"]').children[1].children[0].children[0].children[0].children[streamerNum].children[1].children[1], 5000);
 
 //                  num  <--- this is to be mapped for each streamer
-// streamersDiv.children[0].children[1].insertBefore(starIcon, vcount)
+// streamersDiv.children[streamerNum].children[1].insertBefore(starIcon, vcount)
 
-setTimeout(() => streamersDiv.children[0].children[1].insertBefore(starIcon, vcount), 8000);
+setTimeout(() => streamersDiv.children[streamerNum].children[1].insertBefore(starIcon, vcount), 8000);
 // this works for editing the content. timeout 5000 ms
 // edit this with a better way later. for now this works in testing
 // setTimeout(() => DeleteFollowingList('[aria-label="Followed Channels"]'), 5000);
--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import DeleteFollowingList from '../public/favorite';
 
 function App() {
   return (
@@ -9,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        {/* <DeleteFollowingList /> */}
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -23,8 +21,5 @@ function App() {
     </div>
   );
 }
-
-
-
 
 export default App;
